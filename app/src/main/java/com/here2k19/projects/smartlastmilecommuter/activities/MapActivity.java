0ca29baa6b974c0c8781c48fa5466e577e3981bf
unknown <deciderstomtom@gmail.com>
2019-05-07 11:48:19 +0530

    first commit

---
+++ app/src/main/java/com/here2k19/projects/smartlastmilecommuter/activities/MapActivity.java
@@ -0,0 +1,131 @@
+package com.here2k19.projects.smartlastmilecommuter.activities;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.SupportMapFragment;
+import com.here2k19.projects.smartlastmilecommuter.R;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MapActivity extends FragmentActivity {
+
+    // permissions request code
+    private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
+
+    /**
+     * Permissions that need to be explicitly requested from end user.
+     */
+    private static final String[] REQUIRED_SDK_PERMISSIONS = new String[] {
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE };
+
+    // map embedded in the map fragment
+    private Map map = null;
+
+    // map fragment embedded in this activity
+    private SupportMapFragment mapFragment = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkPermissions();
+
+    }
+
+    private SupportMapFragment getMapFragment() {
+        return (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapfragment);
+    }
+
+    private void initialize() {
+        setContentView(R.layout.activity_map);
+
+        // Search for the map fragment to finish setup by calling init().
+        mapFragment = getMapFragment();
+
+        // Set up disk cache path for the map service for this application
+        boolean success = com.here.android.mpa.common.MapSettings.setIsolatedDiskCacheRootPath(
+                getApplicationContext().getExternalFilesDir(null) + File.separator + ".here-maps",
+                "com.here2k19.projects.smartlastmilecommuter.MapService");
+
+        if (!success) {
+            Toast.makeText(getApplicationContext(), "Unable to set isolated disk cache path.", Toast.LENGTH_LONG);
+        } else {
+            mapFragment.init(new OnEngineInitListener() {
+                @Override
+                public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
+                    if (error == OnEngineInitListener.Error.NONE) {
+                        // retrieve a reference of the map from the map fragment
+                        map = mapFragment.getMap();
+                        // Set the map center to the Vancouver region (no animation)
+                        map.setCenter(new GeoCoordinate(49.196261, -123.004773, 0.0),
+                                Map.Animation.NONE);
+                        // Set the zoom level to the average between min and max
+                        map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
+                    } else {
+                        System.out.println("ERROR: Cannot initialize Map Fragment");
+                    }
+                }
+            });
+        }
+    }
+
+
+    /**
+     * Checks the dynamically controlled permissions and requests missing permissions from end user.
+     */
+    protected void checkPermissions() {
+        final List<String> missingPermissions = new ArrayList<String>();
+        // check all required dynamic permissions
+        for (final String permission : REQUIRED_SDK_PERMISSIONS) {
+            final int result = ContextCompat.checkSelfPermission(this, permission);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                missingPermissions.add(permission);
+            }
+        }
+        if (!missingPermissions.isEmpty()) {
+            // request all missing permissions
+            final String[] permissions = missingPermissions
+                    .toArray(new String[missingPermissions.size()]);
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_ASK_PERMISSIONS);
+        } else {
+            final int[] grantResults = new int[REQUIRED_SDK_PERMISSIONS.length];
+            Arrays.fill(grantResults, PackageManager.PERMISSION_GRANTED);
+            onRequestPermissionsResult(REQUEST_CODE_ASK_PERMISSIONS, REQUIRED_SDK_PERMISSIONS,
+                    grantResults);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                for (int index = permissions.length - 1; index >= 0; --index) {
+                    if (grantResults[index] != PackageManager.PERMISSION_GRANTED) {
+                        // exit the app if one permission is not granted
+                        Toast.makeText(this, "Required permission '" + permissions[index]
+                                + "' not granted, exiting", Toast.LENGTH_LONG).show();
+                        finish();
+                        return;
+                    }
+                }
+                // all permissions were granted
+                initialize();
+                break;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/here2k19/projects/smartlastmilecommuter/activities/Waypoints.java
@@ -0,0 +1,50 @@
+package com.here2k19.projects.smartlastmilecommuter.activities;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.here2k19.projects.smartlastmilecommuter.R;
+import com.nokia.maps.restrouting.GeoCoordinate;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class Waypoints {
+Context context;
+    public void getWaypoints(List<GeoCoordinate> latLngList)
+    {
+       // Context context = ;
+        String app_id=context.getResources().getString(R.string.app_id);
+        String app_code=context.getResources().getString(R.string.app_name);
+        String url="https://wse.api.here.com/2/findsequence.json\n" +
+                "?start=Berlin-Main-Station;52.52282,13.37011\n" +
+                "&destination1=East-Side-Gallery;52.50341,13.44429\n" +
+                "&destination2=Olympiastadion;52.51293,13.24021\n" +
+                "&end=HERE-Berlin-Campus;52.53066,13.38511\n" +
+                "&mode=fastest;car\n" +
+                "&app_id="+app_id +
+                "&app_code="+app_code;
+        RequestQueue requestQueue= Volley.newRequestQueue(context);
+        JsonObjectRequest jsonArrayRequest=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.e("response",response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("err",error.getMessage());
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+
+    }
+}
